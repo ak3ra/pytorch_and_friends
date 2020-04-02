8f205cdef0dc26fd2a37586236cb9b8d09b04f86
BenjaminAkera <akeraben@gmail.com>
2020-04-01 23:37:22 -0400

    added splits

--- multiclass_classification.py
+++ multiclass_classification.py
@@ -1,13 +1,15 @@
 '''
-We use the iris dataset to classify different iris species using a multi-layered perceptron
+We use the iris dataset to classify different 
+iris species using a multi-layered perceptron
 '''
 
 from numpy import vstack
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import random_split
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
-
+from sklearn.preprocessing import test_train_split
 
 class CSVDataset(Dataset):
     # Load the dataset
@@ -28,6 +30,14 @@ class CSVDataset(Dataset):
     #Get an item 
     def __getitem__(self, idx):
         return [self.X[idx], self.y[idx]]
+    
+    ## get indices for train/test split
+    def get_splits(self, n_test=0.33):
+        ## Determine sizes
+        test_size = round(n_test * len(self.X))
+        train_size = len(self.X) - test_size
+        ## calculate the split
+        return random_split(self,[train_size, test_size])
 
      
 
