b002bc4245d4560cf0043390a6e65904d89fca1a
ak3ra <akeraben@gmail.com>
2020-04-06 22:51:56 -0400

    data prep

--- convnet_mnist.py
+++ convnet_mnist.py
@@ -45,18 +45,25 @@ class CNN(Module):
         X = self.act4(X)
         return X
 
+def prepare_data(path):
+    trans = Compose([ToTensor()])
+    ## Download and define the datasets
+    train = MNIST(path, train=True, download=True, transform = trans)
+    test = MNIST(path, train =False, download = True, transform = trans)
+    ## Define how to enumerate the datasets
+    train_dl = DataLoader(train,batch_size = 32, shuffle=True)
+    test_dl = DataLoader(test, batch_size= 32, shuffle=False)
+    ## How to enumerate the datasets
+    return train_dl, test_dl
+
+def train_model(train_dl, model):
+    criterion = MSELoss
+
 
 ## Location to save or load the data
 path = '~/.torch/datasets/mnist'
 ## Transforms to perform to the images
-trans = Compose([ToTensor()])
-## Download and define the datasets
-train = MNIST(path, train=True, download=True, transform = trans)
-test = MNIST(path, train =False, download = True, transform = trans)
-## Define how to enumerate the datasets
-train_dl = DataLoader(train,batch_size = 32, shuffle=True)
-test_dl = DataLoader(test, batch_size= 32, shuffle=True)
-## How to enumerate the datasets
+
 i, (inputs, targets) = next(enumerate(train_dl))
 ## plot some images
 for i in range(25):
