121776f0c68367f89fae9a3db77d5ce9a425da58
ak3ra <akeraben@gmail.com>
2020-04-06 22:36:19 -0400

    convlayers

--- convnet_mnist.py
+++ convnet_mnist.py
@@ -2,7 +2,29 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, ToTensor
 from matplotlib import pyplot
+from torch.nn import module 
+from torch.nn.init import kaiming_uniform_
+from torch.nn import ReLU
+from torch.nn import Conv2d
 
+
+class CNN(Module):
+    def __init__(self, n_channels)
+    super(CNN,self).__init__()
+    self.hidden1 = Conv2d(n_channels, 32, (3,3))
+    kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
+    self.act1 = ReLU()
+    self.pool1 = MaxPool2d((2,2), stride=(2,2))
+    self.hidden2 = Conv2d(32,32,(3,3))
+    kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
+    self.act2 = ReLU()
+    self.pool2 = MaxPool2d((2,2), stride=(2,2))
+    self.hidden3 = Linear(5*5*32, 100)
+    kaiming_uniform_(self.hidden3.weight, nonlinearity='relu')
+    self.act3 = ReLU()
+    self.hidden4 = Linear(100, 10)
+    xavier_uniform_(self.hidden4.weight)
+    self.act4 = SoftMax(dim=1)
 
 ## Location to save or load the data
 path = '~/.torch/datasets/mnist'
