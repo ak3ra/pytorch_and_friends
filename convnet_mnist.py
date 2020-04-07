7384827055d286dd9b5f08ee60572861e3a9803b
ak3ra <akeraben@gmail.com>
2020-04-06 22:49:51 -0400

    forward pass

--- convnet_mnist.py
+++ convnet_mnist.py
@@ -1,30 +1,50 @@
+import numpy as np
+import pandas as pd
+from sklearn.metrics import accuracy_score
 from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import Compose, ToTensor
 from matplotlib import pyplot
-from torch.nn import module 
 from torch.nn.init import kaiming_uniform_
 from torch.nn import ReLU
 from torch.nn import Conv2d
+from torch.nn import Linear,CrossEntropyLoss, Module
 
 
 class CNN(Module):
-    def __init__(self, n_channels)
-    super(CNN,self).__init__()
-    self.hidden1 = Conv2d(n_channels, 32, (3,3))
-    kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
-    self.act1 = ReLU()
-    self.pool1 = MaxPool2d((2,2), stride=(2,2))
-    self.hidden2 = Conv2d(32,32,(3,3))
-    kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
-    self.act2 = ReLU()
-    self.pool2 = MaxPool2d((2,2), stride=(2,2))
-    self.hidden3 = Linear(5*5*32, 100)
-    kaiming_uniform_(self.hidden3.weight, nonlinearity='relu')
-    self.act3 = ReLU()
-    self.hidden4 = Linear(100, 10)
-    xavier_uniform_(self.hidden4.weight)
-    self.act4 = SoftMax(dim=1)
+    def __init__(self, n_channels): 
+        super(CNN,self).__init__()
+        self.hidden1 = Conv2d(n_channels, 32, (3,3))
+        kaiming_uniform_(self.hidden1.weight, nonlinearity='relu')
+        self.act1 = ReLU()
+        self.pool1 = MaxPool2d((2,2), stride=(2,2))
+        self.hidden2 = Conv2d(32,32,(3,3))
+        kaiming_uniform_(self.hidden2.weight, nonlinearity='relu')
+        self.act2 = ReLU()
+        self.pool2 = MaxPool2d((2,2), stride=(2,2))
+        self.hidden3 = Linear(5*5*32, 100)
+        kaiming_uniform_(self.hidden3.weight, nonlinearity='relu')
+        self.act3 = ReLU()
+        self.hidden4 = Linear(100, 10)
+        xavier_uniform_(self.hidden4.weight)
+        self.act4 = SoftMax(dim=1)
+
+    def forward(self, X):
+        X = self.hidden1(X)
+        X = self.act1(X)
+        X = self.pool1(X)
+        X = self.hidden2(X)
+        X = self.act2(X)
+        X = self.pool2(X)
+        ## Flatten
+        X = X.view(-1, 4*4*50)
+        X = self.hidden3(X)
+        X = self.act3(X)
+        ## Output
+        X = self.hidden4(X)
+        X = self.act4(X)
+        return X
+
 
 ## Location to save or load the data
 path = '~/.torch/datasets/mnist'
