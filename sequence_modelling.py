1049d64c4a5a04bffd786d0cc356fafff656feda
ak3ra <akeraben@gmail.com>
2020-04-08 23:56:36 -0400

    Data prep

--- sequence_modelling.py
+++ sequence_modelling.py
@@ -33,3 +33,13 @@ training_data = [
 ]
 
 word_to_ix = {}
+for sent, tags in training_data:
+    for word in sent:
+        if word not in word_to_ix:
+            word_to_ix[word] = len(word_to_ix)
+print(word_to_ix)
+tag_to_idx = {"DET":0, "NN":1, "V":2}
+
+EMBEDDING_DIM = 6
+HIDDEN_DIM = 6
+
