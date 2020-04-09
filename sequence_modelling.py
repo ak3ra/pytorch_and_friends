2f6274c28913e3c191b09f08e14b8f49e7658184
ak3ra <akeraben@gmail.com>
2020-04-08 23:42:35 -0400

    i/o

--- sequence_modelling.py
+++ sequence_modelling.py
@@ -3,3 +3,13 @@ import torch.nn as nn
 import torch.nn.functional as F 
 import torch.optim as optim 
 
+torch.matmul.seed(1)
+
+lstm = nn.LSTM(3,3)
+inputs = [torch.randn(1,3) for _ in range(5)]
+
+hidden = (torch.randn(1,1,3),
+        torch.randn(1,1,3))
+
+for i in inputs:
+    out, hidden = lstm(i.view(1,1,-1), hidden)
