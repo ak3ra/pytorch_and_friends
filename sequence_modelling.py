d73cec0509487105a263e7b862f5b8eca9cc8009
ak3ra <akeraben@gmail.com>
2020-04-11 23:34:20 -0400

    added forward

--- sequence_modelling.py
+++ sequence_modelling.py
@@ -48,7 +48,15 @@ HIDDEN_DIM = 6
 class LSTMTagger(nn.Module):
     def __init__(self,embedding_dim, hidden_dim,vocab_size, tagset_size):
         super(LSTMTagger, self).__init__()
-        self.hiddend_dim = hiddend_dim
+        self.hidden_dim = hidden_dim
         self.word_embedding = nn.Embedding(vocab_size, embedding_dim)
         ## to complete
         self.lstm = nn.Linear(embedding_dim, hidden_dim)
+        self.hidden2tag = nn.Linear(hidden_dim, tagset_size)
+
+    def forward(self, sentence):
+        embeds = self.word_embeddings(sentence)
+        lstm_out,_ = self.lstm(embeds.view(len(sentence), 1, -1))
+        tag_space = self.hidden2tag(lstm_out.view(len(sentence),-1))
+        tag_scores = F.log_softmax(tag_space, dim=1)
+        return tag_scores
