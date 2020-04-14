d20963f2a047fe93520ef1b37beb05f1b0cf68ce
BenjaminAkera <akeraben@gmail.com>
2020-04-13 22:31:22 -0400

    More Parameter examples

--- hooks.py
+++ hooks.py
@@ -17,5 +17,18 @@ class net(nn.Module):
         return self.linear(x)
 
 myNet = net()
-print(list(myNet.parameters())) 
+#print(list(myNet.parameters())) 
 
+###### net 2 ###############
+
+class net2(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv = nn.Linear(10, 5)
+        self.tens = nn.Parameter(torch.ones(3,4))
+
+    def forward(self, x):
+        return self.linear(x)
+    
+new_net = net2()
+print(list(new_net.parameters()))
